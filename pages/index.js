0f9647f72771acd9e2dbbb83323ee13068ff36ed
David Ricaud <davidaricaud@gmail.com>
2022-12-17 23:46:32 +0000

    update icon fix hydration issues

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,43 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>fOotBoNgS n B3eRbalL</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body class="center bg2 max">
-        <div id="main">
-        <h1 class="tab blink intro">FootBongs and Beerball</h1>
+      <main className="center max">
+          <div id="main">
+          <h1 className="tab blink intro">FootBongs and Beerball</h1>
 
-        <Image
-          src="/2022-league-loser.gif"
-          width={650}
-          height={150}
-          priority
-        />
-        <br></br>
-        <Image
-          src="/trent.png"
-          width={600}
-          height={700}
-          priority
-        />
-        <br></br>
-        <Image
-          src="/trent-glitter.gif"
-          width={650}
-          height={150}
-          priority
-        />
-        
-      </div>
-      </body>
+          <Image
+            src="/2022-league-loser.gif"
+            alt='your 2022 league loser'
+            width={650}
+            height={150}
+            priority
+          />
+          <br></br>
+          <Image
+            src="/trent.png"
+            alt='a picture of the league loser (trent)'
+            width={600}
+            height={700}
+            priority
+          />
+          <br></br>
+          <Image
+            src="/trent-glitter.gif"
+            alt='glitter text that says trent bailey'
+            width={650}
+            height={150}
+            priority
+          />
+          
+        </div>
+      </main>
     </>
   )
 }
