8023c2b90988f1fa0471eb45df14c3e42cc79c9f
David Ricaud <davidaricaud@gmail.com>
2022-12-18 04:21:44 +0000

    hopefully made the layout responsive enough

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import loserTitle from '../public/2022-league-loser.gif'
+import loserName from '../public/trent-glitter.gif'
+import loser from '../public/trent-smaller.png'
+
+function img({ src, width, height }) {
+  const style = { paddingBottom: `min(350px, ${100 / (width / height)}%)` }
+  return (
+    <div className={`next-image-wrapper`} style={style}>
+      <Image className="next-image" src={src} fill/>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <>
@@ -8,36 +21,64 @@ export default function Home() {
         <title>fOotBoNgS n B3eRbalL</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="center max">
-          <div id="main">
+      <main className="center">
+          <div>
           <h1 className="tab blink intro">FootBongs and Beerball</h1>
-
-          <Image
-            src="/2022-league-loser.gif"
-            alt='your 2022 league loser'
-            width={650}
-            height={150}
-            priority
-          />
-          <br></br>
-          <Image
-            src="/trent.png"
-            alt='a picture of the league loser (trent)'
-            width={600}
-            height={700}
-            priority
-          />
-          <br></br>
-          <Image
-            src="/trent-glitter.gif"
-            alt='glitter text that says trent bailey'
-            width={650}
-            height={150}
-            priority
-          />
-          
-        </div>
+          </div>
+          <div className="content center">
+          <div className="image-container center imageText">
+              <Image
+                src={loserTitle}
+                alt='your 2022 league loser'
+                fill
+                className={'image'}
+              />
+            </div>
+            <div className="image-container center imagePic">
+              <Image
+                src={loser}
+                alt='a picture of the league loser (trent)'
+                fill
+                className={'image'}
+              />
+            </div>
+            <div className="image-container center imageText">
+              <Image
+                src={loserName}
+                alt='glitter text that says trent bailey'
+                fill
+                className={'image'}
+              />
+            </div>
+         </div>
       </main>
     </>
   )
 }
+
+/*
+x<div className="image-container center">
+              <Image
+                src={loserTitle}
+                alt='your 2022 league loser'
+                fill
+                className={'image'}
+              />
+            </div>
+            <div className="image-container center">
+              <Image
+                src={loser}
+                alt='a picture of the league loser (trent)'
+                fill
+                className={'image'}
+              />
+            </div>
+            <div className="image-container center">
+              <Image
+                src={loserName}
+                alt='glitter text that says trent bailey'
+                fill
+                className={'image'}
+              />
+            </div>
+            */
