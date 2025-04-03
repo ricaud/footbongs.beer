c57c12e68d27cd76a3bf97de06bedc6199869dd1
David Ricaud <david.ricaud@wnco.com>
2025-04-03 14:24:14 -0500

    rename players to golfers and fix typo

--- pages/api/draft/getGolfer.js
+++ pages/api/draft/getGolfer.js
@@ -6,13 +6,13 @@ const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
 export default async function handler(req, res) {
     console.log(req.query)
 
-    //get all players
+    //get all golfers
     if(!req.query.tournament && !req.query.friend) {
         const rows = await sql`SELECT * FROM masters2025players`;
         res.status(200).json(rows);
     }
 
-    //get players active in a certain tournament
+    //get golfers active in a certain tournament
     if(req.query.tournament && !req.query.friend) {
         const rows = await sql`
         SELECT 
@@ -25,13 +25,13 @@ export default async function handler(req, res) {
                 )
             ) AS "golfers"
         FROM draftlog dl
-        JOIN masters2025players p ON p.name = dl.player
+        JOIN masters2025players p ON p.name = dl.golfer
         WHERE tournament=${req.query.tournament} GROUP BY dl.tournament`
 
         res.status(200).json(rows[0]);
     }
 
-    //get players owned by a certain freind in a tounament
+    //get golfers owned by a certain freind in a tounament
     if(req.query.tournament && req.query.friend){
         console.log("here")
 
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
             dl.drafter, 
             JSON_AGG(p.*) AS "golfers"
         FROM draftlog dl
-        JOIN masters2025players p ON p.name = dl.player
+        JOIN masters2025players p ON p.name = dl.golfer
         WHERE tournament=${req.query.tournament} AND drafter=${req.query.friend}
         GROUP BY dl.tournament, dl.drafter`
 
--- pages/api/draft/getTurn.js
+++ pages/api/draft/getTurn.js
@@ -4,6 +4,6 @@ const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
 
 // This function can now leverage a shared connection pool
 export default async function handler(req, res) {
-    const rows = await sql`'SELECT *, friends[curentDrafterIndex+1] as "currentDrafter" FROM draftState WHERE tournament=${req.query.tournament}`;
+    const rows = await sql`'SELECT *, friends[currentDrafterIndex+1] as "currentDrafter" FROM draftState WHERE tournament=${req.query.tournament}`;
     res.status(200).json(rows[0]);
 }
